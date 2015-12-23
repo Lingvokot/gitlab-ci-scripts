4b92f4d36bbf419f85671268cf930b36fbd65ce2
ColCh <colchgh@gmail.com>
2015-12-23 17:32:35 +0300

    Slightly refactor install.sh script

--- install.sh
+++ install.sh
@@ -9,14 +9,43 @@
 # Change working dir to current
 cd "$(dirname "$0")"
 
-# One-liner to install puppet
-curl -sS  https://raw.githubusercontent.com/hashicorp/puppet-bootstrap/master/ubuntu.sh | sh
+# Installs puppet
+function prepare-puppet {
+  # One-liner to install puppet
+  curl -sS  https://raw.githubusercontent.com/hashicorp/puppet-bootstrap/master/ubuntu.sh | sh
 
-# Remove templatedir config option to prevent deprecation warning
-sed -e '/templatedir/ s/^#*/#/' -i.back /etc/puppet/puppet.conf
+  # Remove templatedir config option to prevent deprecation warning
+  sed -e '/templatedir/ s/^#*/#/' -i.back /etc/puppet/puppet.conf
+}
 
-# install gem for hiera secret variables
-gem install hiera-eyaml
+# Check for provision dependencies
+function check_deps {
 
-# Execute our manifest
-puppet apply --verbose --debug --hiera_config ./puppet/hiera.yaml --modulepath ./puppet/modules ./puppet/init.pp
+  if ! type "puppet" > /dev/null; then
+    prepare-puppet
+  fi
+
+  if ! type "eyaml" > /dev/null; then
+    # install gem for hiera secret variables
+    gem install hiera-eyaml
+  fi
+
+}
+
+function apply-manifest {
+  # Execute our manifest
+  puppet apply --verbose --debug --hiera_config ./puppet/hiera.yaml --modulepath ./puppet/modules ./puppet/$1.pp
+}
+
+# Entry point
+function main {
+  check_deps
+  apply-manifest $1
+}
+
+# Let it go
+if [ -n "$1" ]; then
+  main $1
+else
+  echo "Usage: install.sh init"
+fi
