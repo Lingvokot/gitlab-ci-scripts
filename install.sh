5b4fcdd46344f7b1f86a74d0ecfcff64b591b375
ColCh <colchgh@gmail.com>
2016-02-12 19:52:04 +0300

    Redirect stdout depending on DEBUG env variable
    For logging purposes
    
    Related to #3

--- install.sh
+++ install.sh
@@ -9,38 +9,45 @@
 # Change working dir to puppet dir
 cd "$(dirname "$0")/puppet"
 
+# Dynamic stdout redirect
+# Use DEBUG=true to enable debugging
+stdout="/dev/null"
+if [[ $DEBUG ]]; then
+  stdout="/dev/stdout"
+fi
+
 # Installs puppet
 prepare_puppet () {
   # this function is from:
   # https://raw.githubusercontent.com/hashicorp/puppet-bootstrap/master/ubuntu.sh
   echo "Configuring PuppetLabs repo..."
-  wget -q http://apt.puppetlabs.com/puppetlabs-release-pc1-trusty.deb >/dev/null
-  dpkg -i puppetlabs-release-pc1-trusty.deb >/dev/null
-  apt-get update >/dev/null
+  wget -q http://apt.puppetlabs.com/puppetlabs-release-pc1-trusty.deb >$stdout
+  dpkg -i puppetlabs-release-pc1-trusty.deb >$stdout
+  apt-get update >$stdout
 
   echo "Installing Puppet..."
-  DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install puppet-agent >/dev/null
+  DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install puppet-agent >$stdout
 
   echo "Puppet installed!"
 
   # Install RubyGems for the provider
   echo "Installing RubyGems..."
   gem install --no-ri --no-rdoc rubygems-update
-  update_rubygems >/dev/null
+  update_rubygems >$stdout
 }
 
 # Check for provision dependencies
 check_deps () {
 
   # Ruby 2.2dev needed by fastlane
-  if ! which ruby > /dev/null 2>&1 || [[ ! "$(ruby -v)" == *"ruby 2.2"* ]]; then
+  if ! which ruby >$stdout 2>&1 || [[ ! "$(ruby -v)" == *"ruby 2.2"* ]]; then
     echo "Installing ruby version 2.2 ..."
-    apt-get install -y software-properties-common >/dev/null
-    apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
-    apt-get update  >/dev/null
-    apt-get install -qq -y ruby2.2 ruby2.2-dev ruby-switch >/dev/null
+    apt-get install -y software-properties-common >$stdout
+    apt-add-repository -y ppa:brightbox/ruby-ng >$stdout 2>&1
+    apt-get update  >$stdout
+    apt-get install -qq -y ruby2.2 ruby2.2-dev ruby-switch >$stdout
     echo "Switch system ruby version to 2.2"
-    ruby-switch --set ruby2.2 >/dev/null
+    ruby-switch --set ruby2.2 >$stdout
   fi
 
   if [ ! -f "/opt/puppetlabs/bin/puppet" ] ; then
@@ -48,20 +55,20 @@ check_deps () {
     prepare_puppet
   fi
 
-  if ! which librarian-puppet > /dev/null 2>&1; then
+  if ! which librarian-puppet >$stdout 2>&1; then
     echo "Librarian-puppet command is not found. Installing librarian-puppet gem..."
     # install gem for puppet modules
-    gem install librarian-puppet >/dev/null
+    gem install librarian-puppet >$stdout
   fi
 
-  if ! which eyaml > /dev/null 2>&1; then
+  if ! which eyaml >$stdout 2>&1; then
     # install gem for hiera secret variables
     echo "Eyaml command is not found. Installing hiera-eyaml gem..."
     # for puppet
-    /opt/puppetlabs/puppet/bin/gem install hiera-eyaml >/dev/null
+    /opt/puppetlabs/puppet/bin/gem install hiera-eyaml >$stdout
     ln -s /opt/puppetlabs/puppet/bin/eyaml /opt/puppetlabs/bin/eyaml
     # system-wide
-    gem install hiera-eyaml >/dev/null
+    gem install hiera-eyaml >$stdout
   fi
 
 }
