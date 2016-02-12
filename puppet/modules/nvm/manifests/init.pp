43e6a3aa03d2fd7da80466c818547ea628202197
ColCh <colchgh@gmail.com>
2016-02-12 19:28:03 +0300

    Remove NPM_CONFIG_PREFIX from profile.d file
    This was a legacy code

--- puppet/modules/nvm/manifests/init.pp
+++ puppet/modules/nvm/manifests/init.pp
@@ -31,8 +31,5 @@ file { '/etc/profile.d/nvm.sh':
   content => '
 export NVM_DIR=/usr/local/nvm
 source /opt/nvm/nvm.sh
-
-export NPM_CONFIG_PREFIX=/usr/local/node
-export PATH="/usr/local/node/bin:$PATH"
   ',
 }
