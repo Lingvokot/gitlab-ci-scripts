dc30760822f87c76c892a8e753b5b897a461ac31
ColCh <colchgh@gmail.com>
2016-02-12 20:13:58 +0300

    Add build-export script for gitlab runner manifest
    Resolves #9

--- puppet/manifests/gitlab-runner.pp
+++ puppet/manifests/gitlab-runner.pp
@@ -5,3 +5,10 @@ class { 'gitlab_runner':
     token => hiera('gitlab_runner::token'),
     description => hiera('gitlab_runner::description'),
 }
+
+# Place build-export.sh script
+file { '/opt/build-export.sh':
+  ensure => file,
+  mode => 'ug+x',
+  source => 'puppet:///modules/scripts/build-export.sh',
+}
