3e4c57c01e00edb99352368df7d9a5e5ae79e65f
ColCh <colchgh@gmail.com>
2016-04-11 17:26:50 +0300

    Install JDK 7 instead of 6 one Close #18

--- puppet/modules/jdk/manifests/init.pp
+++ puppet/modules/jdk/manifests/init.pp
@@ -13,7 +13,7 @@ class jdk {
     command => 'echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections',
     path => ["/bin", "/usr/bin", "/usr/sbin"],
   } ~>
-  package { 'oracle-java6-installer':
+  package { 'oracle-java7-installer':
     ensure => 'installed',
   }
 
